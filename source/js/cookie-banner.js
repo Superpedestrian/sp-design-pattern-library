c6a9cbb67bc7fead55cbcf8836281bfa82831dd7
Joe <joe@superpedestrian.com>
2018-04-30 11:12:11 -0400

    Changed design of cookie header to use accept/decline

--- source/js/cookie-banner.js
+++ source/js/cookie-banner.js
@@ -2,4 +2,10 @@ function cookiesAccepted() {
   var banner = document.getElementById("cookie-banner");
   banner.style.display = "none";
   document.cookie = "cookies-accepted=yes;path=/"
+  location.reload(true);
+}
+
+function cookiesDeclined() {
+  var banner = document.getElementById("cookie-banner");
+  banner.style.display = "none";
 }
