b44761eaaa4995aca0d2738621b1da95ffe96baf
Joe <joe@superpedestrian.com>
2018-05-08 15:34:27 -0400

    Cookie domain is updated

--- source/js/cookie-banner.js
+++ source/js/cookie-banner.js
@@ -1,8 +1,8 @@
 function cookiesAccepted() {
   var banner = document.getElementById("cookie-banner");
   banner.style.display = "none";
-  document.cookie = "cookies-accepted=yes;path=/"
-  document.cookie = "cookies-send-accepted=yes;path=/"
+  document.cookie = "cookies-accepted=yes;path=/;domain=.superpedestrian.com";
+  document.cookie = "cookies-send-accepted=yes;path=/;domain=.superpedestrian.com";
   location.reload(true);
 }
 
